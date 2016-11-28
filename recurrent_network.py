fc058db7ab868e434ac0ad80487c60b5d53a1211
James Reed <jamesr66@vt.edu>
2016-11-28 07:56:19 -0500

    somewhat reasonable results

--- recurrent_network.py
+++ recurrent_network.py
@@ -27,13 +27,13 @@ handle 28 sequences of 28 steps for every sample.
 
 # Parameters
 learning_rate = 0.001
-training_iters = 100000
+training_iters = 500000
 batch_size = 128
 display_step = 10
 
 # Network Parameters
-n_input = 28 # MNIST data input (img shape: 28*28)
-n_steps = 28 # timesteps
+n_input = 28*28 # MNIST data input (img shape: 28*28)
+n_steps = 1 # timesteps
 n_hidden = 128 # hidden layer num of features
 n_classes = 10 # MNIST total classes (0-9 digits)
 
@@ -96,6 +96,7 @@ with tf.Session() as sess:
         batch_x, batch_y = mnist.train.next_batch(batch_size)
         # Reshape data to get 28 seq of 28 elements
         batch_x = batch_x.reshape((batch_size, n_steps, n_input))
+        batch_x = batch_x + np.random.randn(batch_size, n_steps, n_input) * 0.5 + 0.5
         # Run optimization op (backprop)
         sess.run(optimizer, feed_dict={x: batch_x, y: batch_y})
         if step % display_step == 0:
@@ -116,20 +117,24 @@ with tf.Session() as sess:
     print("Testing Accuracy:", \
         sess.run(accuracy, feed_dict={x: test_data, y: test_label}))
 
-    imtest = np.expand_dims(test_data[1, :, :], 0)
-    scipy.misc.imsave('out/base.jpg', imtest[0,:,:])
-    labeltest = np.zeros((1, 10))
-    labeltest[0, 0] = 1.
-    #labeltest = np.expand_dims(test_label[0], 0)
-    print(labeltest, labeltest.shape)
+    for num in range(10):
+      imtest = np.expand_dims(test_data[1, :, :], 0)
+      scipy.misc.imsave('out/base.jpg', imtest[0,:,:])
+      labeltest = np.zeros((1, 10))
+      labeltest[0, num] = 1.
+      #labeltest = np.expand_dims(test_label[0], 0)
+      print(labeltest, labeltest.shape)
 
-    for idx in xrange(10):
-      var_grad = tf.gradients(cost, [x])[0]
-      imgrad = sess.run(var_grad, feed_dict={x: imtest, y: labeltest})
-      loss = sess.run(cost, feed_dict={x: imtest, y: labeltest})
+      for idx in xrange(5):
+        var_grad = tf.gradients(cost, [x])[0]
+        imgrad = sess.run(var_grad, feed_dict={x: imtest, y: labeltest})
+        loss = sess.run(cost, feed_dict={x: imtest, y: labeltest})
 
-      adjusted = imtest - 10*imgrad
-      scipy.misc.imsave('out/adjusted{}.jpg'.format(idx), adjusted[0,:,:].repeat(2, axis=0).repeat(2, axis=1))
+        adjusted = (imtest - 10*imgrad)
+        scipy.misc.imsave(
+          'out/adjusted{}_{}.jpg'.format(num, idx),
+          np.reshape(adjusted[0,:,:], (28, 28)).repeat(2, axis=0).repeat(2, axis=1)
+        )
 
-      print('loss', loss, 'delta', np.linalg.norm(adjusted[0,:,:] - imtest[0,:,:]))
-      imtest = adjusted
+        print('loss', loss, 'delta', np.linalg.norm(adjusted[0,:,:] - imtest[0,:,:]))
+        imtest = adjusted
