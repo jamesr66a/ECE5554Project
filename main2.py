8e1176deab3e06d1983c6c5e2665c8d6354a8c53
James Reed <jamesr66@vt.edu>
2016-11-30 07:35:29 -0500

    it does what we expect it to

--- main2.py
+++ main2.py
@@ -4,14 +4,17 @@ import scipy.misc
 from tensorflow.models.image.cifar10 import cifar10
 from util import *
 import tensorflow as tf
+from skimage.color import rgb2gray
 
 def load_cifar_data(path):
   d = unpickle(path)
   data = d['data']
   labels = np.array(d['labels'])
-  data = data.reshape((-1, 3, 1024))
-  data = np.sqrt(np.power(data[:, 0, :], 2) + np.power(data[:, 1, :], 2) + np.power(data[:, 2, :], 2))
   data = np.divide(data, 255.)
+  data = data.reshape((-1, 3, 1024))
+  data = np.transpose(data, [0, 2, 1])
+  data = np.reshape(data, [-1, 32, 32, 3])
+  data = rgb2gray(data)
 
   return np.reshape(data, [-1, 32, 32, 1]), labels
 
@@ -23,7 +26,13 @@ def main():
     data, labels = load_cifar_data('cifar-10-batches-py/data_batch_1')
     test_data, test_labels = load_cifar_data('cifar-10-batches-py/test_batch')
 
-    dn.train(data, sess) 
+    dn.train(data, sess)
+
+    dream = dn.dream(np.expand_dims(test_data[0, :, :, :], 0), sess)
+
+    scipy.misc.imsave(
+      'dream.bmp', np.squeeze(dream).repeat(8, axis=0).repeat(8, axis=1)
+    )
 
 if __name__=='__main__':
   main()
