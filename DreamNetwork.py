b5ecb99f78774111067861bbb9bf43da5fb03039
James Reed <jamesr66@vt.edu>
2016-12-15 15:04:05 -0500

    move

--- DreamNetwork.py
+++ DreamNetwork.py
@@ -4,218 +4,124 @@ import numpy as np
 import scipy.misc
 
 import tensorflow as tf
+
 from tensorflow.python.ops import rnn, rnn_cell
 
 from pixelrnntensorflow.ops import *
 
 import os
 
-class DreamNetwork:
-  def __init__(self, learning_rate=0.001, lstm_depth=5, n_input=32, n_steps=32,\
-               n_hidden=128, n_classes=10, forget_bias=1.0, out_recurrent_length=2,
-               out_hidden_dims=32):
-    self.learning_rate = learning_rate
-    self.lstm_depth = lstm_depth
-    self.n_input = n_input
-    self.n_steps = n_steps
-    self.n_hidden = n_hidden
-    self.n_classes = n_classes
+height = width = 32
+channel = 1
+
+class DreamNetwork2:
+  def __init__(self, hidden_dims=16, recurrent_length=3, out_recurrent_length=2, out_hidden_dims=32, learning_rate=5e-3, dream_learning_rate=1e-3):
+    self.hidden_dims = hidden_dims
+    self.recurrent_length = recurrent_length
     self.out_recurrent_length = out_recurrent_length
     self.out_hidden_dims = out_hidden_dims
+    self.learning_rate = learning_rate
+    self.dream_learning_rate = dream_learning_rate
 
-    self.arch = {}
-
-  def PixelRNN(self, x, weights, biases, reuse):
+  def PixelRNN(self, x, reuse, dream=False, sess=None):
+    print('Initialize Network')
     with tf.variable_scope('pixelrnn', reuse=reuse):
-      conv_inputs = conv2d(x, self.n_hidden * 2, [7,7], "A", scope='pixelrnn')
+      conv_inputs = conv2d(x, self.hidden_dims *  2, [7,7], 'A', scope='conv_inputs')
 
       class meme(object):
         pass
-  
+
       conf = meme()
       setattr(conf, 'use_residual', True)
-      setattr(conf, 'hidden_dims', self.n_hidden)
+      setattr(conf, 'hidden_dims', self.hidden_dims)
       setattr(conf, 'use_dynamic_rnn', False)
 
       l_hid = conv_inputs
-      recs = {}
-      # main recurrent layers
-      for idx in xrange(self.lstm_depth):
-        scope = 'LSTM{}'.format(idx)
-        recs[scope] = l_hid = diagonal_bilstm(l_hid, conf, scope=scope)
+      for idx in xrange(self.recurrent_length):
+        l_hid = diagonal_bilstm(l_hid, conf, scope='LSTM{}'.format(idx))
 
-      # output recurrent layers
       for idx in xrange(self.out_recurrent_length):
-        scope = 'CONV_OUT{}'.format(idx)
-        l_hid = tf.nn.relu(conv2d(l_hid, self.out_hidden_dims, [1,1], 'B', scope=scope))
+        l_hid = tf.nn.relu(conv2d(l_hid, self.out_hidden_dims, [1,1], 'B', scope='CONV_OUT{}'.format(idx)))
 
       conv2d_out_logits = conv2d(l_hid, 1, [1,1], 'B', scope='conv2d_out_logits')
-
       output = tf.nn.sigmoid(conv2d_out_logits)
 
-      return output 
+      loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
+        conv2d_out_logits, x, name='loss'))
 
-  def RNN(self, x, weights, biases, reuse):
-    x = tf.transpose(x, [1, 0, 2])
-    x = tf.reshape(x, [-1, self.n_input])
-    x = tf.split(0, self.n_steps, x)
+      global_step = tf.Variable(0, trainable=False)
+      lr = tf.train.exponential_decay(self.learning_rate, global_step, 500, 0.80)
+      optimizer = tf.train.RMSPropOptimizer(lr)
+      grads_and_vars = optimizer.compute_gradients(loss)
+      new_grads_and_vars = [(tf.clip_by_value(gv[0], -1, 1), gv[1]) for gv in grads_and_vars]
 
-    with tf.variable_scope('rnn', reuse=reuse):
-      lstm = rnn_cell.BasicLSTMCell(self.n_hidden//2, forget_bias=1.0)
-      stacked_lstm = rnn_cell.MultiRNNCell([lstm] * self.lstm_depth)
-      stacked_lstm_bw = rnn_cell.MultiRNNCell([lstm] * self.lstm_depth)
-      outputs, _, _ = rnn.bidirectional_rnn(stacked_lstm, stacked_lstm_bw, x, dtype=tf.float32)
+      if dream:
+        temp = set(tf.all_variables())
+        optimizer_dream = tf.train.AdamOptimizer(
+          learning_rate=self.dream_learning_rate
+        ).minimize(loss, var_list=[x])
+        sess.run(tf.initialize_variables(set(tf.all_variables()) - temp))
+      else:
+        optimizer_dream = None
 
-    return tf.add(tf.matmul(outputs[-1], weights['out']), biases['out'])
+      optim = optimizer.apply_gradients(new_grads_and_vars)
 
-  def init_network(self, x, reuse=False):
-    with tf.variable_scope('params', reuse=reuse) as scope:
-      weights = {
-        'out': tf.get_variable('weights', [1024, self.n_classes],
-          initializer=tf.random_normal_initializer(0., 1.)
-        )
-      }
-      biases = {
-        'out': tf.get_variable('biases', [1, self.n_classes],
-          initializer=tf.random_normal_initializer(0., 1.)
-        )
-      }
+      return output, optim, loss, optimizer_dream
 
-      #pred = self.RNN(x, weights, biases, reuse)
-      pred = self.PixelRNN(x, weights, biases, reuse)
+  def train(self, x, sess, batch_size=128, niterations=500):
+    inputs = tf.placeholder(tf.float32, [None, height, width, channel])
 
-      return pred, weights, biases
+    output, optim, loss, _ = self.PixelRNN(inputs, reuse=False)
 
+    print('initialize variables')
+    sess.run(tf.initialize_all_variables())
 
-  def train(self, x, y, sess, testx, testy, batch_size=100, training_iters=10000000, display_step=10, test_step=100, savepath=None): 
-    xs = tf.placeholder('float32', [None, self.n_steps, self.n_input, 1])
-    ys = tf.placeholder('float32', [None, self.n_classes])
-
-    pred, weights, biases = self.init_network(xs)
-    print(pred.get_shape())
-
-    cost = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(pred, xs))
-    optimizer = tf.train.RMSPropOptimizer(learning_rate=self.learning_rate)
-    grads_and_vars = optimizer.compute_gradients(cost)
-    new_grads_and_vars = \
-        [(tf.clip_by_value(gv[0], -1, 1), gv[1]) for gv in grads_and_vars]
-    optim = optimizer.apply_gradients(new_grads_and_vars) 
-
-    correct_pred = tf.equal(pred, xs)
-    accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
-
-    if savepath is not None and os.path.exists(savepath):
-      saver = tf.train.Saver()
-      saver.restore(sess, savepath)
-      print("Model successfully restored from {}".format(savepath))
-      return
-
-    init = tf.initialize_all_variables()
-    sess.run(init)
-
-    testx = np.reshape(testx, (-1, self.n_steps, self.n_input, 1))
-    nex, _, _, _ = testx.shape
-    testlabels = np.zeros((nex, 10))
-    for idx in xrange(nex):
-      testlabels[idx, testy[idx]] = 1.  
-
-    step = 1
+    nex, _, _, _ = x.shape
     try:
-      while step*batch_size < training_iters:
-        data_size, _, _, _ = x.shape
-        idxs = np.random.choice(data_size, batch_size)
-        batch_x = x[idxs, :]
-        ls = y[idxs]
-        batch_y = np.zeros((batch_size, self.n_classes))
-        for idx in xrange(batch_size):
-          batch_y[idx, ls[idx]] = 1.
-        batch_x = batch_x.reshape((batch_size, self.n_steps, self.n_input, 1))
-        sess.run(optim, feed_dict={xs: batch_x, ys: batch_y})
-        if step % display_step == 0:
-          acc = sess.run(accuracy, feed_dict={xs: batch_x, ys: batch_y})
-          #acc = 0.0
-          loss = sess.run(cost, feed_dict={xs: batch_x, ys: batch_y})
-          print("Iter " + str(step*batch_size) + ", Minibatch Loss= " + \
-                "{:.6f}".format(loss) + ", Training Accuracy= " + \
-                "{:.5f}".format(acc))
-        #if step % test_step == 0:
-          print("Testing Accuracy:", \
-            sess.run(accuracy, feed_dict={xs: testx, ys: testlabels}))
-        step += 1
+      for idx in xrange(niterations):
+        x_batch = x[np.random.choice(nex, batch_size), :, :, :]
+        _, cost = sess.run([
+          optim, loss,
+        ], feed_dict={ inputs: x_batch })
+        print('Iteration: ', idx, 'Loss: ', cost)
     except KeyboardInterrupt:
       pass
-    print("Optimization complete!")
 
-    saver = tf.train.Saver()
-    saver.save(sess, savepath)
-    print("Model saved to {}".format(savepath))
- 
-  def dream(self, x, y, sess, training_iters=500, display_step=10,learning_rate=.1):
-    x = np.reshape(x, (-1, self.n_steps, self.n_input, 1))
-    xs = tf.Variable(x, dtype=np.float32)
-    ys = tf.placeholder('float32', [None, self.n_classes])
+  def dream(self, x, sess, niterations=1000):
+    x = np.reshape(x, [-1, 32, 32, 1])
+    inputs = tf.Variable(x, dtype=np.float32)
 
-    pred, weights, biases = self.init_network(xs, reuse=True)
+    _, _, loss, optim_dream = self.PixelRNN(inputs, reuse=True, dream=True, sess=sess)
 
-    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred, x))
+    sess.run(tf.initialize_variables([inputs]))
 
-    global_step = tf.Variable(0, trainable=False)
-    lr = tf.train.exponential_decay(learning_rate, global_step, 1000, 0.5)
-    optimizer = tf.train.AdamOptimizer(
-      learning_rate=lr
-    ).minimize(cost, var_list=[xs], global_step=global_step)
+    try:
+      for idx in xrange(niterations):
+        _, cost = sess.run([
+          optim_dream, loss
+        ])
+        print('Dream iteration: ', idx, 'Loss: ', cost)
+        if idx % 10 == 0:
+          scipy.misc.imsave(
+            'out/dream{}.bmp'.format(idx), np.squeeze(sess.run(inputs)).repeat(8, axis=0).repeat(8, axis=1)
+          )
 
-    correct_pred = tf.equal(tf.argmax(pred, 1), tf.argmax(ys, 1))
-    accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+        if cost <= 0:
+          break
+    except KeyboardInterrupt:
+      pass
 
-    to_init = [xs]
+    return sess.run(inputs)
+
+  def test(self, x, sess):
+    inputs = tf.placeholder(tf.float32, [None, height, width, channel])
+
+    output, optim, loss, _ = self.PixelRNN(inputs, reuse=True)
+
+    to_init = [inputs]
     for var in tf.all_variables():
       if not sess.run(tf.is_variable_initialized(var)):
         to_init.append(var)
-
-    sm = tf.nn.softmax(pred)
-
     sess.run(tf.initialize_variables(to_init))
 
-    step = 1
-    while True:
-      nex, _, _, _ = x.shape
-      batch_y = np.zeros((nex, 10))
-      for idx in xrange(nex):
-        batch_y[idx, y[idx]] = 1.
-      sess.run(optimizer, feed_dict={ys: batch_y})
-      p = sess.run(sm, feed_dict={ys: batch_y})
-      if step % display_step == 0:
-        acc = sess.run(accuracy, feed_dict={ys: batch_y})
-        loss = sess.run(cost, feed_dict={ys: batch_y})
-        print("Iter " + str(step) + ", Minibatch Loss= " + \
-              "{:.6f}".format(loss) + ", Training Accuracy= " + \
-              "{:.5f}".format(acc))
-        print(p)
-      step += 1
-      b = True
-      for i in xrange(nex):
-        if p[i, y[i]] < 0.95:
-          b = False
-      if b:
-        break
-    print("Optimization complete!")
-
-    return sess.run(xs) 
-
-  def test(self, x, y, sess):
-    x = np.reshape(x, (-1, self.n_steps, self.n_input, 1))
-    xs = tf.placeholder('float32', [None, self.n_steps, self.n_input, 1])
-    ys = tf.placeholder('float32', [None, self.n_classes])
-    nex, _, _, _ = x.shape
-    labels = np.zeros((nex, 10))
-    for idx in xrange(nex):
-      labels[idx, y[idx]] = 1.
-
-    pred, weights, biases = self.init_network(xs, reuse=True)
-
-    correct_pred = tf.equal(tf.argmax(pred, 1), tf.argmax(ys, 1))
-    accuracy = tf.reduce_mean(tf.abs(tf.cast(correct_pred, tf.float32)))
-
-    print("Testing Accuracy:", \
-      sess.run(accuracy, feed_dict={xs: x, ys: labels}))
+    return sess.run(loss, feed_dict={ inputs: x })
--- main.py
+++ main.py
@@ -1,48 +1,38 @@
-from DreamNetwork import DreamNetwork
+from DreamNetwork2 import DreamNetwork2
 import numpy as np
 import scipy.misc
 from tensorflow.models.image.cifar10 import cifar10
 from util import *
 import tensorflow as tf
+from skimage.color import rgb2gray
 
 def load_cifar_data(path):
   d = unpickle(path)
   data = d['data']
   labels = np.array(d['labels'])
-  data = np.linalg.norm(data.reshape((-1, 3, 1024)), axis=1)
+  data = np.divide(data, 255.)
+  data = data.reshape((-1, 3, 1024))
+  data = np.transpose(data, [0, 2, 1])
+  data = np.reshape(data, [-1, 32, 32, 3])
+  data = rgb2gray(data)
 
-  return data, labels 
+  return np.reshape(data, [-1, 32, 32, 1]), labels
 
 def main():
   with tf.Session() as sess:
-    dn = DreamNetwork(n_hidden=16, lstm_depth=7, learning_rate=1e-3)
-
-    n_test = 100
+    dn = DreamNetwork2()
 
     savepath = "./model.ckpt"
     data, labels = load_cifar_data('cifar-10-batches-py/data_batch_1')
-    data = np.reshape(data, [-1, 32, 32, 1])
     test_data, test_labels = load_cifar_data('cifar-10-batches-py/test_batch')
-    test_data = np.reshape(test_data, [-1, 32, 32, 1])
-    dn.train(data, labels, sess, test_data[0:n_test, :], test_labels[0:n_test], training_iters=500000, savepath=savepath)
 
-    dn.test(test_data[0:n_test, :], test_labels[0:n_test], sess)
+    dn.train(data, sess)
+
+    dream = dn.dream(np.expand_dims(test_data[0, :, :, :], 0), sess)
 
     scipy.misc.imsave(
-      'orig.jpg', np.squeeze(np.reshape(test_data[0, :], (32, 32)))\
-        .repeat(2, axis=0).repeat(2, axis=1)
+      'dream.bmp', np.squeeze(dream).repeat(8, axis=0).repeat(8, axis=1)
     )
 
-    dream_img = dn.dream(
-      np.tile(np.zeros((32, 32)), 10), xrange(10),
-      sess, learning_rate=1.0, training_iters=100, display_step=1
-    ) 
-
-    for idx in xrange(10):
-      scipy.misc.imsave(
-        'dream{}.bmp'.format(idx), np.squeeze(dream_img)[idx,:,:].repeat(8, axis=0).repeat(8, axis=1)
-      )
-
-
-if __name__ == '__main__':
+if __name__=='__main__':
   main()
